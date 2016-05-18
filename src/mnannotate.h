fa08e4b0dd5c9e6a39477a9d74824668f41049ec
Biyong Chen <bchen@sensity.com>
2016-05-18 14:31:33 -0700

    Add proper liense file and headers for GPL distribution

--- src/mnannotate.h
+++ src/mnannotate.h
@@ -1,7 +1,20 @@
-
-/***********************************************************************
- * Copyright 2016 Sensity Systems Inc. 
- ***********************************************************************/
+/*
+ * This file is part of media-utis
+ *
+ * Media-utils is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Media-utils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with media-utils; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <opencv/cv.h> 
 #include <opencv2/videoio/videoio_c.h>
